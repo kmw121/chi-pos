8e37e327e12a3d3bf4504ffc733e4c8ccc92398c
kmsy120 <kmsy120@naver.com>
2022-12-31 17:04:12 +0900

    .

--- frontend/src/components/SignIn/SignInForm.js
+++ frontend/src/components/SignIn/SignInForm.js
@@ -57,7 +57,17 @@ function SignInForm({ onToggle }) {
       const jwtToken = res.data.data.accessToken;
       const decoded = jwt_decode(jwtToken);
       dispatch(setUser(decoded));
-      setCookie("jwtToken", jwtToken);
+
+      const expires = new Date();
+      expires.setFullYear(expires.getFullYear() + 10);
+
+      setCookie("jwtToken", jwtToken, {
+        path: '/',        // 쿠키 값을 저장하는 서버 경로
+        expires,          // 유효 시간
+        //secure: true,   // 웹 브라우저와 웹 서버가 https로 통신하는 경우에만 쿠키 저장
+        //httpOnly: true  // document.cookie라는 자바스크립트 코드로 쿠키에 비정상적으로 접속하는 것을 막는 옵션
+      });
+
       onToggle();
       setLoginForm((prev) => {
         return { ...prev, username: "", password: "" };
