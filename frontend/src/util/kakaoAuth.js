fa9665a612a5bf261b2023603a49f6f8cdb6d7d6
kmsy120 <kmsy120@naver.com>
2023-01-04 23:31:18 +0900

    카카오 로그인 test용

--- frontend/src/util/kakaoAuth.js
+++ frontend/src/util/kakaoAuth.js
@@ -1,4 +1,4 @@
 const CLIENT_ID = "07030fde952adb60be217a2ef0f85931";
-const REDIRECT_URI = "http://localhost:3000/oauth/callback/kakao";
+const REDIRECT_URI = "http://3.39.164.180:8080/oauth/callback/kakao";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
