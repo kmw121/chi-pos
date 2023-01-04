51561a529c5cfa14893033016cb745fadefffe73
kmsy120 <kmsy120@naver.com>
2023-01-04 22:42:31 +0900

    카카오 로그인 test용

--- src/main/java/f2b2/Comma/controller/OAuthController.java
+++ src/main/java/f2b2/Comma/controller/OAuthController.java
@@ -1,15 +1,19 @@
 package f2b2.Comma.controller;
 
+import f2b2.Comma.domain.oauth.OAuthService;
 import f2b2.Comma.dto.CMRespDto;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class OAuthController {
+
+    private final OAuthService oAuthService;
 
     /**
      * 카카오 callback
@@ -19,6 +23,9 @@ public class OAuthController {
     @GetMapping("/ouath/kakao")
     public ResponseEntity<?> kakaoCallback(@RequestParam String code) {
         System.out.println(code);
-        return new ResponseEntity<>(new CMRespDto<>(1, "코드 받기 성공", code), HttpStatus.OK);
+        String kakaoAccessToken = oAuthService.getKakaoAccessToken(code);
+        String[] kakaoUserInfo = oAuthService.createKakaoUser(kakaoAccessToken);
+
+        return new ResponseEntity<>(new CMRespDto<>(1, "코드 받기 성공", kakaoUserInfo), HttpStatus.OK);
     }
 }
--- src/main/java/f2b2/Comma/domain/oauth/OAuthService.java
+++ src/main/java/f2b2/Comma/domain/oauth/OAuthService.java
@@ -2,6 +2,7 @@ package f2b2.Comma.domain.oauth;
 
 import com.google.gson.JsonParser;
 import com.google.gson.JsonElement;
+import f2b2.Comma.handler.exception.CustomException;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -28,8 +29,8 @@ public class OAuthService{
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
-            sb.append("&client_id=e4a81d5a6acbda948310e08e2eafc123"); // TODO REST_API_KEY 입력
-            sb.append("&redirect_uri=http://localhost:9000/oauth/kakao"); // TODO 인가코드 받은 redirect_uri 입력
+            sb.append("&client_id=07030fde952adb60be217a2ef0f85931"); // TODO REST_API_KEY 입력
+            sb.append("&redirect_uri=http://localhost:3000/oauth/callback/kakao"); // TODO 인가코드 받은 redirect_uri 입력
             sb.append("&code=" + code);
             bw.write(sb.toString());
             bw.flush();
@@ -65,5 +66,59 @@ public class OAuthService{
         }
 
         return access_Token;
+    }
+
+    public String[] createKakaoUser(String token) {
+
+        String reqURL = "https://kapi.kakao.com/v2/user/me";
+
+        //access_token을 이용하여 사용자 정보 조회
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Authorization", "Bearer " + token); //전송할 header 작성, access_token전송
+
+            //결과 코드가 200이라면 성공
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
+            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+            //Gson 라이브러리로 JSON파싱
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            int id = element.getAsJsonObject().get("id").getAsInt();
+            boolean hasEmail = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_email").getAsBoolean();
+            String email = "";
+            if(hasEmail){
+                email = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("email").getAsString();
+            }
+
+            System.out.println("id : " + id);
+            System.out.println("email : " + email);
+
+            br.close();
+
+            String[] idEmail = new String[2];
+
+            idEmail[0] = Integer.toString(id);
+            idEmail[0] = email;
+            return idEmail;
+
+        } catch (IOException e) {
+            throw new CustomException(e.getMessage());
+        }
     }
 }
