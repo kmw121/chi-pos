0a36c2941650732c7e7fc907d0b3d70f45b748db
kmsy120 <kmsy120@naver.com>
2023-01-05 21:12:03 +0900

    카카오 로그인기능

--- frontend/src/util/cookie.js
+++ frontend/src/util/cookie.js
@@ -2,7 +2,7 @@ import { Cookies } from "react-cookie";
 const cookies = new Cookies();
 
 export const setCookie = (name, value) => {
-  return cookies.set(name, value,{path:"/",domain:"http://3.39.164.180:8080/"});
+  return cookies.set(name, value,{domain:"http://3.39.164.180:8080/"});
 };
 
 export const getCookie = (name) => {
