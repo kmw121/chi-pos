fd51c896050c1743aebf2f4880a2153b2f3703cb
ddeongkyu <tjdrb4230@naver.com>
2023-01-05 17:06:08 +0900

    FE-logic-0105

--- frontend/src/components/KakaoSocial.js
+++ frontend/src/components/KakaoSocial.js
@@ -1,15 +1,34 @@
 import axios from "axios";
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { API_URL } from "../util/API_URL";
 import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import { setCookie, deleteCookie } from "../util/cookie";
+import jwt_decode from "jwt-decode";
+import { setUser } from "../slice/userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 const KakaoSocial = () => {
   let code = new URL(window.location.href).searchParams.get("code");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     async function fetchData() {
       try {
         const res = await axios.get(API_URL + `/ouath/kakao?code=${code}`);
         console.log(res);
+        if (res.data.code === 2) {
+          setCookie("Kakao", res.data.data);
+          navigate("/kakaoSignup");
+        } else if (res.data.code === 1) {
+          const jwtToken = res.data.data.accessToken;
+          const refreshToken = res.data.data.refreshToken;
+          const decoded = jwt_decode(jwtToken);
+          dispatch(setUser(decoded));
+          deleteCookie("jwtToken");
+          deleteCookie("refreshToken");
+          setCookie("jwtToken", jwtToken);
+          setCookie("refreshToken", refreshToken);
+        }
       } catch (err) {
         console.log(err);
       }
