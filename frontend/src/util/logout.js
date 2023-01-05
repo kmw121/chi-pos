c7eb68fc33a694499e17a8643165a39dbd04bc6e
kmsy120 <kmsy120@naver.com>
2023-01-05 23:10:11 +0900

    카카오 로그인기능

--- frontend/src/util/logout.js
+++ frontend/src/util/logout.js
@@ -5,6 +5,10 @@ export const logout = (dispatch) => {
   deleteCookie(["refreshToken"]);
   deleteCookie(["jwtToken"]);
   deleteCookie(["refreshToken"]);
+  deleteCookie(["jwtToken"]);
+  deleteCookie(["refreshToken"]);
+  deleteCookie(["jwtToken"]);
+  deleteCookie(["refreshToken"]);
 
   dispatch(setUserInfo([]));
   dispatch(setUser([]));
