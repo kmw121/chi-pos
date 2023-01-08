8a8ea23ede75b70cee92a37e4d74407685e3aa47
kmsy120 <kmsy120@naver.com>
2023-01-08 12:28:29 +0900

    페이지 기능 보수

--- frontend/src/util/cookie.js
+++ frontend/src/util/cookie.js
@@ -3,6 +3,7 @@ const cookies = new Cookies();
 
 export const setCookie = (name, value) => {
   console.log("나 실행 됨?");
+  document.cookie = name +"=" + value+ "; path=/; http://3.39.164.180:8080"
   return cookies.set(name, value);
 };
 
