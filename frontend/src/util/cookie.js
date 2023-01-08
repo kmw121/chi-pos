ebf66fd262592ac15f37ceaf81ff3e62a7ca9174
kmsy120 <kmsy120@naver.com>
2023-01-08 12:22:48 +0900

    페이지 기능 보수

--- frontend/src/util/cookie.js
+++ frontend/src/util/cookie.js
@@ -2,6 +2,7 @@ import { Cookies } from "react-cookie";
 const cookies = new Cookies();
 
 export const setCookie = (name, value) => {
+  console.log("나 실행 됨?");
   return cookies.set(name, value);
 };
 
