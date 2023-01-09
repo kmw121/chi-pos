d8ec6febe8a0c6a513eaa1c3c1aa9a6b0a89044c
kmsy120 <kmsy120@naver.com>
2023-01-10 02:23:11 +0900

    페이지 기능 보수

--- frontend/src/util/logout.js
+++ frontend/src/util/logout.js
@@ -3,14 +3,6 @@ import { setUser, setUserInfo } from "../slice/userSlice";
 export const logout = (dispatch, navigate) => {
   document.cookie = 'jwtToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT; domain=3.39.164.180:8080; path=/;';
   document.cookie = 'refreshToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT; domain=3.39.164.180:8080; path=/;';
-  deleteCookie(["jwtToken"]);
-  deleteCookie(["refreshToken"]);
-  deleteCookie(["jwtToken"]);
-  deleteCookie(["refreshToken"]);
-  deleteCookie(["jwtToken"]);
-  deleteCookie(["refreshToken"]);
-  deleteCookie(["jwtToken"]);
-  deleteCookie(["refreshToken"]);
   deleteCookie("jwtToken");
   deleteCookie("refreshToken");
 
@@ -18,5 +10,11 @@ export const logout = (dispatch, navigate) => {
   dispatch(setUser([]));
   alert("로그아웃되었습니다.");
   navigate("/");
+  deleteCookie("jwtToken");
+  deleteCookie("refreshToken");
+  document.cookie = 'jwtToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT; domain=3.39.164.180:8080; path=/;';
+  document.cookie = 'refreshToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT; domain=3.39.164.180:8080; path=/;';
+  dispatch(setUserInfo([]));
+  dispatch(setUser([]));
   window.location.reload();
 };
