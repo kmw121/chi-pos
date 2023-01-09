2fe2ad3c8965f9be63302d2f61720a55c7656c16
kmsy120 <kmsy120@naver.com>
2023-01-10 01:58:01 +0900

    페이지 기능 보수

--- frontend/src/util/logout.js
+++ frontend/src/util/logout.js
@@ -3,6 +3,8 @@ import { setUser, setUserInfo } from "../slice/userSlice";
 export const logout = (dispatch, navigate) => {
   document.cookie = "jwtToken = ; expires=Thu, 01 Jan 1970 00:00:01 GMT;"
   document.cookie = "refreshToken = ; expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+  document.cookie = 'jwtToken=; expire=날짜; domain=http://3.39.164.180:8080; path=/;';
+  document.cookie = 'refreshToken=; expire=날짜; domain=http://3.39.164.180:8080; path=/;';
   deleteCookie(["jwtToken"]);
   deleteCookie(["refreshToken"]);
   deleteCookie(["jwtToken"]);
