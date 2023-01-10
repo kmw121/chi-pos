bd78045d168762ea17b54a86e6a3c3e3c543fe12
kmsy120 <kmsy120@naver.com>
2023-01-10 21:41:53 +0900

    페이지 기능 보수

--- src/main/java/f2b2/Comma/config/WebMvcConfig.java
+++ src/main/java/f2b2/Comma/config/WebMvcConfig.java
@@ -9,26 +9,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    private String connectPath = "/home/file/**";
-    private String resourcePath = "file:///home/file/";
+//    private String connectPath = "/home/file/**";
+//    private String resourcePath = "file:///home/file/";
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
 
 
-//        registry.addResourceHandler("/**/*")
-//                .addResourceLocations("classpath:/static/")
-//                .resourceChain(true)
-//                .addResolver(new PathResourceResolver() {
-//                    @Override
-//                    protected Resource getResource(String resourcePath,
-//                                                   Resource location) throws IOException {
-//                        Resource requestedResource = location.createRelative(resourcePath);
-//                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-//                                : new ClassPathResource("/static/index.html");
-//                    }
-//                });
-        registry.addResourceHandler(connectPath)
-                .addResourceLocations(resourcePath);
+        registry.addResourceHandler("/**/*")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath,
+                                                   Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
+//        registry.addResourceHandler(connectPath)
+//                .addResourceLocations(resourcePath);
     }
 }
--- src/main/java/f2b2/Comma/dto/auth/OauthSignupDto.java
+++ src/main/java/f2b2/Comma/dto/auth/OauthSignupDto.java
@@ -53,7 +53,7 @@ public class OauthSignupDto {
                 e.printStackTrace();
             }
             System.out.println(imageFilePath);
-            user.setImageUrl(imageFilePath.toString());
+            user.setImageUrl("./file/"+imageFileName);
         }
         else{
             user.setImageUrl("nonUrl");
