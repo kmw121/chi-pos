4361ffada0f7e0e07e122f88f88831ada4359934
kmsy120 <kmsy120@naver.com>
2023-01-11 03:46:07 +0900

    페이지 기능 보수

--- src/main/java/f2b2/Comma/controller/UserController.java
+++ src/main/java/f2b2/Comma/controller/UserController.java
@@ -165,7 +165,7 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Map<String, String> user) {
         User loginUser = userService.login(user.get("username"),user.get("password"));
-        final Path FILE_ROOT = Paths.get("../../../../img").toAbsolutePath().normalize();
+        System.out.println(Paths.get("../../../../img").toAbsolutePath().normalize());
         return new ResponseEntity<>(new CMRespDto<>(1,"로그인 성공", new JwtDto(jwtTokenProvider.createAccessToken(loginUser.getUsername(), loginUser.getRoles(), loginUser.getId()), jwtTokenProvider.createRefreshToken(loginUser.getUsername(), loginUser.getRoles(), loginUser.getId()))),HttpStatus.OK);
     }
 
