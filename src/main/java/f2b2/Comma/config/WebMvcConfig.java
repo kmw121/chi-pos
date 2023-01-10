ef5bca24a36831cde9f1ceb914ad6459693913e3
kmsy120 <kmsy120@naver.com>
2023-01-10 21:16:52 +0900

    페이지 기능 보수

--- src/main/java/f2b2/Comma/config/WebMvcConfig.java
+++ src/main/java/f2b2/Comma/config/WebMvcConfig.java
@@ -25,6 +25,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //                    }
 //                });
         registry.addResourceHandler("/home/ec2-user/file/**")
-                .addResourceLocations("file:///home/ec2-user/file/");
+                .addResourceLocations("file:/home/ec2-user/file/")
+                .resourceChain(true);
     }
 }
