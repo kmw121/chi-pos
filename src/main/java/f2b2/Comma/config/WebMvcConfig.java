cdba5cc5d31f62bc0fee035fc9c46c6c7936bf49
kmsy120 <kmsy120@naver.com>
2023-01-10 21:05:22 +0900

    페이지 기능 보수

--- src/main/java/f2b2/Comma/config/WebMvcConfig.java
+++ src/main/java/f2b2/Comma/config/WebMvcConfig.java
@@ -12,18 +12,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("/**/*")
-                .addResourceLocations("classpath:/static/")
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver() {
-                    @Override
-                    protected Resource getResource(String resourcePath,
-                                                   Resource location) throws IOException {
-                        Resource requestedResource = location.createRelative(resourcePath);
-                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                                : new ClassPathResource("/static/index.html");
-                    }
-                });
+//        registry.addResourceHandler("/**/*")
+//                .addResourceLocations("classpath:/static/")
+//                .resourceChain(true)
+//                .addResolver(new PathResourceResolver() {
+//                    @Override
+//                    protected Resource getResource(String resourcePath,
+//                                                   Resource location) throws IOException {
+//                        Resource requestedResource = location.createRelative(resourcePath);
+//                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+//                                : new ClassPathResource("/static/index.html");
+//                    }
+//                });
         registry.addResourceHandler("/home/ec2-user/file/**")
                 .addResourceLocations("file:///home/ec2-user/file/");
     }
