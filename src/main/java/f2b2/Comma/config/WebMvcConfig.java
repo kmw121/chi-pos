0830b98e7d189b417f1cead69d40a44a53c5fbf8
kmsy120 <kmsy120@naver.com>
2023-01-11 04:18:57 +0900

    페이지 기능 보수

--- src/main/java/f2b2/Comma/config/WebMvcConfig.java
+++ src/main/java/f2b2/Comma/config/WebMvcConfig.java
@@ -1,39 +1,28 @@
-package f2b2.Comma.config;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.PathResourceResolver;
-@Configuration
-@EnableWebMvc
-public class WebMvcConfig implements WebMvcConfigurer {
-
-//    final Path FILE_ROOT = Paths.get("../../img").toAbsolutePath().normalize();
-//    private String connectPath = "/upload/**";
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler(connectPath)
-//                .addResourceLocations("file:///home/ec2-user/commaProject/img/");
-
-        registry.addResourceHandler("/**/*")
-                .addResourceLocations("classpath:/static/")
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver() {
-                    @Override
-                    protected Resource getResource(String resourcePath,
-                                                   Resource location) throws IOException {
-                        Resource requestedResource = location.createRelative(resourcePath);
-                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                                : new ClassPathResource("/static/index.html");
-                    }
-                });
-
-    }
-}
+//package f2b2.Comma.config;
+//
+//import java.io.IOException;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.core.io.Resource;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.resource.PathResourceResolver;
+//@Configuration
+//@EnableWebMvc
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+////    final Path FILE_ROOT = Paths.get("../../img").toAbsolutePath().normalize();
+////    private String connectPath = "/upload/**";
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+////        registry.addResourceHandler(connectPath)
+////                .addResourceLocations("file:///home/ec2-user/commaProject/img/");
+//
+//
+//
+//    }
+//}
--- src/main/java/f2b2/Comma/config/provider/FileConfig.java
+++ src/main/java/f2b2/Comma/config/provider/FileConfig.java
@@ -1,10 +1,14 @@
 package f2b2.Comma.config.provider;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
@@ -20,5 +24,18 @@ public class FileConfig implements WebMvcConfigurer {
             registry.addResourceHandler("/upload/**")
                     .addResourceLocations("file:///home/ec2-user/commaProject/img/")
                     .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+
+            registry.addResourceHandler("/**/*")
+                    .addResourceLocations("classpath:/static/")
+                    .resourceChain(true)
+                    .addResolver(new PathResourceResolver() {
+                        @Override
+                        protected Resource getResource(String resourcePath,
+                                                       Resource location) throws IOException {
+                            Resource requestedResource = location.createRelative(resourcePath);
+                            return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                    : new ClassPathResource("/static/index.html");
+                        }
+                    });
         }
 }
