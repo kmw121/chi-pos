db4abd1676942a123acc78845d6e73803f043109
kmsy120 <kmsy120@naver.com>
2023-01-11 02:07:47 +0900

    페이지 기능 보수

--- src/main/java/f2b2/Comma/config/WebMvcConfig.java
+++ src/main/java/f2b2/Comma/config/WebMvcConfig.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
     private String connectPath = "/file/**";
     private String resourcePath = "file:///home/ec2-user/file/";
