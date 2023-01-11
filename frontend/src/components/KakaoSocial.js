b05fda8160288da722ee88219e2bdbb1f75d2524
kmsy120 <kmsy120@naver.com>
2023-01-12 01:44:11 +0900

    페이지 기능 보수

--- frontend/src/components/KakaoSocial.js
+++ frontend/src/components/KakaoSocial.js
@@ -37,9 +37,11 @@ const KakaoSocial = () => {
           deleteCookie("refreshToken");
 
           document.cookie = "jwtToken" +" = " + jwtToken+ "; path=/;"
-          console.log("토큰생성1");
+          console.log("토큰생성1.1");
           document.cookie = "refreshToken" +" = " + refreshToken+ "; path=/;"
-          console.log("토큰생성2");
+          console.log("토큰생성2.1");
+
+          window.location.href='http://chi-pos.com/';
 
 
         }
