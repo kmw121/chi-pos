d3477f85f86d279bcdfaf3e04ac3df53fdafdc4c
kmsy120 <kmsy120@naver.com>
2023-01-25 07:21:51 +0900

    코드 리팩터링 : alert 제거(2023/01/25)

--- frontend/src/util/logout.js
+++ frontend/src/util/logout.js
@@ -3,9 +3,7 @@ import { setUser, setUserInfo, setCurrentPost } from "../slice/userSlice";
 export const logout = (dispatch, navigate) => {
   navigate("/");
   document.cookie = "jwtToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;";
-  document.cookie =
-    "refreshToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;";
-  alert(document.cookie);
+  document.cookie = "refreshToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;";
   deleteCookie("jwtToken");
   deleteCookie("refreshToken");
   dispatch(setUserInfo([]));
